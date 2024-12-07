71d388ff3a71af8ed035c6e8d37fffa56d6a01dc
tellerlin <lintao_mailbox@163.com>
2024-12-07 16:10:08 +0800

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -81,8 +81,7 @@
             <p>&copy; {new Date().getFullYear()} ByteSlim. All rights reserved.</p>
             <nav aria-label="Footer Navigation">
                 <ul>
-                    <li><a href="/privacy">Privacy Policy</a></li>
-                    <li><a href="/terms">Terms of Service</a></li>
+                    <li><a href="/about">ABOUT</a></li>
                 </ul>
             </nav>
         </div>
