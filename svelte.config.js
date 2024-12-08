2b9034276331942adbd64f488d92f8bd7aa3ec66
tellerlin <lintao_mailbox@163.com>
2024-12-08 11:49:32 +0800

    sitemap8

--- svelte.config.js
+++ svelte.config.js
@@ -1,37 +1,37 @@
-export const prerender = true;
+// svelte.config.js
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 
-const site = 'https://www.byteslim.com';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    csp: {
+      directives: {
+        'default-src': ['self', 'https:'],
+        'script-src': ['self', 'unsafe-inline', 'unsafe-eval'],
+        'style-src': ['self', 'unsafe-inline'],
+        'img-src': ['self', 'data:', 'blob:', 'https:'],
+        'connect-src': ['self', 'blob:'],
+        'media-src': ['self', 'blob:', 'https:'],
+        'frame-src': ['self'],
+        'font-src': ['self', 'https:', 'data:']
+      }
+    },
+    prerender: {
+      entries: [
+        '/',
+        '/about',
+        '/privacy', 
+        '/contact'
+      ],
+      handleHttpError: 'warn',
+      handleMissingId: 'warn'
+    }
+  }
+};
 
 
-const pages = [
-    { path: '/', priority: '1.0', changefreq: 'daily' },
-    { path: '/about', priority: '0.8', changefreq: 'weekly' },
-    { path: '/privacy', priority: '0.6', changefreq: 'monthly' },
-    { path: '/contact', priority: '0.7', changefreq: 'weekly' }
-];
-
-
-export async function GET() {
-    const urlset = pages.map(page => `
-    <url>
-        <loc>${site}${page.path === '/' ? '' : page.path}</loc>
-        <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-        <changefreq>${page.changefreq}</changefreq>
-        <priority>${page.priority}</priority>
-    </url>`).join('');
-
-
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urlset}
-</urlset>`;
-
-
-    return new Response(sitemap, {
-        headers: {
-            'Content-Type': 'application/xml; charset=utf-8',
-            'Cache-Control': 'max-age=0, s-maxage=3600'
-        }
-    });
-}
+export default config;
